9a1c928b661a7ba1d90172f7bcb9120c4870b233
Web EGSPGOI <web@egspec.org>
2026-02-25 04:34:53 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@s

--- src/components/shared/smooth-scroll.tsx
+++ src/components/shared/smooth-scroll.tsx
@@ -1,37 +1,43 @@
 'use client';
 
-import { ReactLenis, useLenis } from '@studio-freight/react-lenis';
+import Lenis from '@studio-freight/lenis';
 import { usePathname } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 function SmoothScroll({ children }: { children: React.ReactNode }) {
-  const lenisRef = useRef<any>(null);
+  const lenisRef = useRef<Lenis | null>(null);
   const pathname = usePathname();
 
-  useLenis(lenis => {
-    // Integrate with GSAP
+  useEffect(() => {
+    const lenis = new Lenis();
+    lenisRef.current = lenis;
+
+    const update = (time: number) => {
+      lenis.raf(time * 1000);
+    }
+    
+    gsap.registerPlugin(ScrollTrigger);
     lenis.on('scroll', ScrollTrigger.update);
 
-    gsap.ticker.add((time) => {
-        lenis.raf(time * 1000);
-    });
-
+    gsap.ticker.add(update);
     gsap.ticker.lagSmoothing(0);
-  })
-
+    
+    return () => {
+        gsap.ticker.remove(update);
+        lenis.destroy();
+        lenisRef.current = null;
+    }
+  }, []);
+  
   useEffect(() => {
     if (lenisRef.current) {
-      lenisRef.current.scrollTo(0, { immediate: true });
+        lenisRef.current.scrollTo(0, { immediate: true });
     }
   }, [pathname]);
 
-  return (
-    <ReactLenis root ref={lenisRef}>
-      {children}
-    </ReactLenis>
-  );
+  return children;
 }
 
 export default SmoothScroll;
